403fb4dee9c5064fc8b81490f68903300711cd29
Andrey Koltsov <a99920641@gmail.com>
2019-10-10 11:33:31 +0300

    Rename biosnoop process.

--- venv/biosnoop-statistics-visualization.py
+++ venv/biosnoop-statistics-visualization.py
@@ -3,9 +3,9 @@ import subprocess
 
 BIOSNOOP_MOCK_SCRIPT_NAME = "biosnoop-output-mock.sh"
 
-test_forever_output = subprocess.Popen(['./{biosnoop_mock}'.format(biosnoop_mock=BIOSNOOP_MOCK_SCRIPT_NAME)], shell=True, stdout=subprocess.PIPE)
-while test_forever_output.poll() is None:
-    pollingResult = test_forever_output.stdout
+biosnoop_process = subprocess.Popen(['./{biosnoop_mock}'.format(biosnoop_mock=BIOSNOOP_MOCK_SCRIPT_NAME)], shell=True, stdout=subprocess.PIPE)
+while biosnoop_process.poll() is None:
+    pollingResult = biosnoop_process.stdout
     line = pollingResult.readline().decode("utf-8")
     print(line)
     time.sleep(0.5)
