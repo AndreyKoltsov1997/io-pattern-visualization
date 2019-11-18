4bc45e8e51b9354c89019db160337b01ca4d6e48
AndreyKoltsov1997 <a99920641@gmail.com>
2019-11-18 12:34:05 +0300

    Remove parsing.

--- venv/biosnoop-statistics-visualization.py
+++ venv/biosnoop-statistics-visualization.py
@@ -54,54 +54,17 @@ def visualize_io_pattern(amount_of_logs_to_collect, source_file_name):
         hovertext.append(list())
         # NOTE: Getting the process name by its ID
         print(f"heatmap_pid_values[yi]: {heatmap_pid_values[yi]}")
-        process_name_fetch_result_raw = subprocess.run(
-            ['ls'.format(proc_id=heatmap_pid_values[yi])], shell=True, stdout=subprocess.PIPE)
-        process_name_fetch_result = process_name_fetch_result_raw.stdout.decode("utf-8").split()
-        residentSetSize = "unknown"
-        processName = "unknown"
+
         cpu = "unknown"
         virtualMemorySize = "unknown"
         ps_cmd_raw_result = []
-        print(f"process_name_fetch_result: {process_name_fetch_result}")
-        target_process_name = ""
-        if (len(process_name_fetch_result) > 0):
-            if (process_name_fetch_result[0] != "PID"):
-                target_process_name = process_name_fetch_result[0]
-                ps_cmd_result = subprocess.run(
-                    ['ls'.format(process_name=target_process_name)],
-                    shell=True,
-                    stdout=subprocess.PIPE)
-                ps_cmd_raw_result = ps_cmd_result.stdout.decode("utf-8").split()
-        else:
-            target_process_name = "unknown"
-
-        if (len(ps_cmd_raw_result) > 0):
-            # NOTE: Retrieving the info about the process
-            ps_cmd_result = subprocess.run(
-                ['ls'.format(process_name=target_process_name)], shell=True,
-                stdout=subprocess.PIPE)
-            ps_cmd_raw_result = ps_cmd_result.stdout.decode("utf-8").split()
-            if (len(ps_cmd_raw_result) == 0):
-                continue
-            # @param residentSetSize - RSS is the Resident Set Size and is used to show how much memory is allocated to that process and is in RAM.
-            # It does not include memory that is swapped out.
-            # It does include memory from shared libraries as long as the pages from those libraries are actually in memory.
-            # It does include all stack and heap memory.
-            residentSetSize = ps_cmd_raw_result[5]
-
-            # @param virtualMemorySize - VSZ is the Virtual Memory Size. It includes all memory that the process can access, ..
-            # ...including memory that is swapped out, memory that is allocated, but not used, and ..
-            # ... memory that is from shared libraries.
-            virtualMemorySize = ps_cmd_raw_result[4]
-
-            cpu = ps_cmd_raw_result[2]
-            processName = ps_cmd_raw_result[0]
+        target_process_name = "unknown"
 
         for xi, xx in enumerate(heatmap_x_values):
             hovertext[-1].append(
                 'Process name: {} <br />Time: {}<br />Latency: {}<br />Bytes: {} <br /> PID: {}, CPU: {} <br /> Virtual Memory Size: {} <br /> Resident Set Size: {}'.format(
-                    target_process_name, xx, yy, heatmap_z_labels[0][yi], heatmap_pid_values[yi], cpu,
-                    virtualMemorySize, residentSetSize))
+                    target_process_name, xx, yy, heatmap_z_labels[0][yi], heatmap_pid_values[yi], "unknown",
+                    "unknown", "unknown"))
 
     io_pattern_heat_map = go.Figure(data=go.Heatmap(
         z=heatmap_z_values,
